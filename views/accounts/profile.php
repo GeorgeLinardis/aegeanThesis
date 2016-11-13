8de30756c613363331a09ceab37f5fcf763a4a01
GeorgeLinardis <glinardis@gmail.com>
2016-11-13 21:17:47 +0200

    added profile page

--- views/accounts/profile.php
+++ views/accounts/profile.php
@@ -1,32 +1,28 @@
-<div class="container">
-    <div class="row" >
+<?php use yii\widgets\DetailView ;
+?>
 
-        <div class="col-sm-offset-4 col-sm-6">
-            <div class="text-center"><h3>Προφίλ Χρήστη</h3></div><br />
-            <?php $this->widget('zii.widgets.CDetailView',array(
-                'data'=>$model,
-                'htmlOptions'=>array('class'=>'table table-striped'),
-                'attributes'=>array(
-                    "userUsername",
-                    'firstname',
-                    "lastname",
-                    array('label'=>"Email",
-                          'value'=>CHtml::encode($model->email),
-                          'type'=>'email'),
-                    "telephone",
-                    array('label'=>'URL',
-                          'value'=> CHtml::encode($model->url),
-                          'type'=> "url",
-                         ),
+<div class="row">
 
-            )));?>
-        </div>
+    <div class="col-sm-8">
+        <h3 class="text-center">Προφίλ Χρήστη</h3><br />
+        <?php
+            echo DetailView::widget([
+            'model' => $model,
+            'attributes'=>[
+                ['label'=>'Όνομα Χρήστη',
+                 'value'=>$model->userUsername,
+                ],
+                'firstname',
+                'lastname',
+                'telephone',
+                'email',
+                'url'
+                ],
+
+             'options'=>['class'=>'table table-striped'],
+
+            ]);
+
+    ?>
     </div>
-
-
-
-
-
-    <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-
 </div>
