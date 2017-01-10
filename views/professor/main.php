9edd9568dc70681dd3cb871235698bd194436aa0
GeorgeLinardis <glinardis@gmail.com>
2017-01-10 19:06:04 +0200

    graphics for professor main page ok

--- views/professor/main.php
+++ views/professor/main.php
@@ -1,19 +1,44 @@
 <?php
 use yii\helpers\Html;
-use app\models\Professor;
+use yii\helpers\Url;
 ?>
 
 <?php
 $this->title = 'Καθηγητής';
-
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
+
 <div class="professor-main">
+    <div class="row">
+        <div class="col-sm-6">
+            <a href= "<?= Url::to('/professor/thesis')?>" class="thumbnail">
+                <img src="/images/professor/Professors-main-page-thesis" alt="Theses">
+            </a>
+            <h3> Οι Διπλωματικές μου </h3>
+        </div>
 
-    <h1><?= Html::encode($this->title) ?></h1>
+        <div class="col-sm-6">
+            <a href="<?= Url::to('/professor/committee')?>" class="thumbnail">
+                <img src="/images/professor/Professors-main-page-committees" alt="Committees">
+            </a>
+            <h3> Επιτροπές τρίτων</h3>
+        </div>
+    </div><br>
 
-Καλησπέρα σας <?= \app\CustomHelpers\UserHelpers::UserFullName(); ?>
+    <div class="row">
+        <div class="col-sm-6">
+            <a href= "<?= Url::to('/professor/statistics')?>" class="thumbnail">
+                <img src="/images/professor/Professors-main-page-statistics(Pixabay)" alt="Statistics">
+            </a>
+            <h3> Στατιστικά</h3>
+        </div>
 
-
+        <div class="col-sm-6">
+            <a href="<?= Url::to('/student/all-theses')?>" class="thumbnail">
+                <img src="/images/professor/Professors-main-page-references" alt="My References">
+            </a>
+            <h3> Πηγές φοιτητών μου</h3>
+        </div>
+    </div>
 </div>
