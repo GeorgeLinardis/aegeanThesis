f5199945d213bc8d3ad240d827204ac7e82c61f1
GeorgeLinardis <glinardis@gmail.com>
2017-01-29 01:25:18 +0200

    added thesis_applications menu

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -71,6 +71,7 @@ AppAsset::register($this);
                             ['label' => 'Η διπλωματική μου', 'url' => ['student/my-thesis']],
                             ['label' => 'Οι πηγές μου', 'url' => ['student/my-references']],
                             ['label' => 'Λίστα διπλωματικών', 'url' => ['student/my-master-theses']],
+                            ['label' => 'Αιτήσεις νέας διπλωματικής', 'url' => ['student/thesis-application-results']],
                             ['label' => 'Επικοινωνια με καθηγητή', 'url' => ['student/student-chat']],
                         ],
                     ]
