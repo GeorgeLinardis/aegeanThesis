cf10157d047fb94a08d4921afcf427f70e2d29b8
GeorgeLinardis <glinardis@gmail.com>
2017-02-02 00:08:20 +0200

    automated some student menus

--- views/student/main.php
+++ views/student/main.php
@@ -11,7 +11,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="student-all-theses">
     <h1> Αρχική σελίδα <?= rtrim(Html::encode($this->title),"ς") ?></h1><br>
         <div class="row">
-            <div class="col-sm-2">
+            <div class="col-sm-2 col-sm-offset-1">
                 <a href= "<?= Url::to('/student/my-thesis')?>" class="thumbnail">
                     <img src="/images/student/student-mythesis(pexels).jpg" alt="My Thesis">
                 </a>
@@ -25,15 +25,16 @@ $this->params['breadcrumbs'][] = $this->title;
                 <h4> Οι πηγές μου</h4>
             </div>
             <div class="col-sm-2">
-                <a href= "<?= Url::to('/thesis/index')?>" class="thumbnail">
+                <a href= "<?= Url::to('/thesis/index')?>" class="thumbnail" >
                     <img src="/images/student/student-alltheses" alt="All theses">
                 </a>
                 <h4> Λίστα διπλωματικών</h4>
             </div>
             <div class="col-sm-2">
-                <a href="<?= Url::to(['//chat/chat-room','ThesisID'=>$StudentThesisID])?>" class="thumbnail">
-                    <img src="/images/student/student-chat(pixabay).jpg" alt="Professor-chat">
-                </a>
+                    <a href="<?=Url::to(['//chat/chat-room','ThesisID'=>$StudentThesisID])?>" style="<?= ($StudentThesisID==false)? 'cursor:not-allowed':'' ?>" class="thumbnail">
+                        <img src="/images/student/student-chat(pixabay).jpg" alt="Professor-chat">
+                    </a>
+
                 <h4> Επικοινωνία με καθηγητή</h4>
             </div>
             <div class="col-sm-2">
@@ -42,11 +43,6 @@ $this->params['breadcrumbs'][] = $this->title;
                 </a>
                 <h4> Αιτήσεις νέας διπλωματικής</h4>
             </div>
-            <div class="col-sm-2">
-                <a href="<?= Url::to('/student/student-chat')?>" class="thumbnail">
-                    <img src="/images/student/student-chat(pixabay).jpg" alt="Professor-chat">
-                </a>
-                <h4> Άλλα</h4>
-            </div>
+
 </div>
 </div>
