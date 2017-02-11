9e524eaee9509701862e49476b5dd3538d88a8f4
GeorgeLinardis <glinardis@gmail.com>
2017-02-11 11:21:31 +0200

    changed email to committee appearance

--- mail/committee-approval-email.php
+++ mail/committee-approval-email.php
@@ -1,15 +1,17 @@
 <?php
 use yii\helpers\Html;
 use yii\helpers\Url;
+use app\models\Master;
 ?>
-
+<?php $MasterTitle = Master::find()->where(['ID'=>$PostData['Thesis']['masterID']])->one() ?>
 <div class="row">
-    <p>Θα ήθελα να συμμετέχετε στην παρακάτω επιτροπή:</p>
-    <ul>
-        <li>Τίτλος: <?php echo $PostData['Thesis']['title']?></li><br>
-        <li>Περιγραφή: <?php echo $PostData['Thesis']['description']?></li><br>
-        <li>Πρόγραμμα σπουδών: <?php echo $PostData['Thesis']['masterID']?></li>
+    <p>Γειά σας,</p>
 
+    <p>Θα ήθελα να συμμετέχετε στην Επιτροπή της παρακάτω διπλωματικής:</p>
+    <ul>
+        <li><b>Τίτλος:</b> <?php echo $PostData['Thesis']['title']?></li><br>
+        <li><b>Περιγραφή:</b> <?php echo $PostData['Thesis']['description']?></li><br>
+        <li><b>Πρόγραμμα σπουδών:</b> <?php echo $MasterTitle->title?></li>
     </ul>
 
 
@@ -17,7 +19,7 @@ use yii\helpers\Url;
     <p>Με εκτίμηση</p>
     <b><?php echo $Sender->firstname.' '.$Sender->lastname?></b>
 
-
+<hr>
 <br><br>
 <p><b>Αποδοχή αιτήματος</b><br>
     Στην περίπτωση που <u>ενδιαφέρεστε</u> να συμμετάσχετε στην Επιτροπή μπορείτε να αποδεχθείτε το αίτημα επιλέγοντας τον ακόλoυθο σύνδεσμο:</p>
