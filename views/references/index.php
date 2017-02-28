4dbb374892520b7bc0bed3876247f8bc4dc71773
GeorgeLinardis <glinardis@gmail.com>
2017-02-28 22:08:28 +0200

    description of page added

--- views/references/index.php
+++ views/references/index.php
@@ -18,41 +18,45 @@ elseif (Yii::$app->controller->id=="professor"){
 $this->title = 'Πηγές φοιτητών μου';
 $this->params['breadcrumbs'][] = $this->title;}
 ?>
-<div class="references-index">
+<div class="row">
+    <div class="references-index">
 
-        <h1><?= Html::encode($this->title) ?></h1><br>
-    <p>Επιλέξτε το σύμβολο <span class="glyphicon glyphicon-eye-open" style="color:#0080ff"></span> στην δεξιά στήλη για να δείτε τα στοιχεία της αναφοράς αναλυτικότερα.</p><br>
+            <h1><?= Html::encode($this->title) ?></h1><br>
+        <p>Παρακάτω εμφανίζονται οι βιβλιογραφικές αναφορές των φοιτητών του Πανεπιστημίου ανεξαρτήτως τμήματος.</p>
 
-    <?php  //echo $this->render('_search', ['model' => $searchModel]); ?>
-    <p>
-        <?php
-        if (Yii::$app->controller->id == 'student'){
-        echo Html::a('Δημιουργία Αναφοράς', ['references/create'], ['class' => 'btn btn-success']);
-        }; ?>
-    </p>
+        <p>Επιλέξτε το σύμβολο <span class="glyphicon glyphicon-eye-open" style="color:#0080ff"></span> στην δεξιά στήλη για να δείτε τα στοιχεία της αναφοράς αναλυτικότερα.</p><br>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            //['class' => 'yii\grid\SerialColumn'],
-            'ID',
-            'title',
-            'author',
-            'type',
-            //'professorID',
-            //'URL:url',
-             'date_created_by_author:date',
-             //'date_created_by_student:date',
-             //'date_updated_by_student:date',
-            // 'file:ntext',
+        <?php  //echo $this->render('_search', ['model' => $searchModel]); ?>
+        <p>
+            <?php
+            if (Yii::$app->controller->id == 'student'){
+            echo Html::a('Δημιουργία Αναφοράς', ['references/create'], ['class' => 'btn btn-success']);
+            }; ?>
+        </p>
 
-            ['class' => 'yii\grid\ActionColumn',
-                'template' => '{view}',
-                'controller'=>'references'
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'columns' => [
+                //['class' => 'yii\grid\SerialColumn'],
+                'ID',
+                'title',
+                'author',
+                'type',
+                //'professorID',
+                //'URL:url',
+                 'date_created_by_author:date',
+                 //'date_created_by_student:date',
+                 //'date_updated_by_student:date',
+                // 'file:ntext',
 
-                ],
+                ['class' => 'yii\grid\ActionColumn',
+                    'template' => '{view}',
+                    'controller'=>'references'
 
-        ],
-    ]); ?>
+                    ],
+
+            ],
+        ]); ?>
+    </div>
 </div>
--- views/thesis/index.php
+++ views/thesis/index.php
@@ -16,59 +16,63 @@ elseif (Yii::$app->controller->id=="student"){
     $this->params['breadcrumbs'][]=['label'=>'Φοιτητής','url'=>'main'];
     $this->params['breadcrumbs'][] = $this->title;
 } ?>
+<div class="row">
+    <div class="row thesis-index">
 
-<div class="thesis-index">
+
+        <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+        <?php if (Yii::$app->controller->id=="thesis"):?>
+        <h1><?= Html::encode($this->title) ?></h1><br>
+            <p>Παρακάτω μπορείτε να δείτε όλες τις διπλωματικές του Πανεπιστημίου.</p>
+        <?php endif;?>
+
+        <p>Επιλέξτε το σύμβολο <span class="glyphicon glyphicon-eye-open" style="color:#0080ff"></span> στην δεξιά στήλη για να δείτε τα στοιχεία της διπλωματικής αναλυτικότερα.</p><br>
+    </div>
+    <div class="row">
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'columns' => [
+                //['class' => 'yii\grid\SerialColumn'],
+                'ID',
+
+                //'masterID',
+                [
+                    'attribute' => 'masterID',
+                    'value' => ('master.title')
+                ],
+
+                //'professorID',
+                [
+                    'attribute' => 'professorID',
+                    'value'=>function ($model) {
+                        return $model->professor->lastname.' '. $model->professor->firstname;
+                    },
+                ],
+
+                'title',
+                //'description:ntext',
+                //'goal:ntext',
+                // 'prerequisite_knowledge:ntext',
+                // 'max_students',
+                // 'comments:ntext',
+                 'status',
+                 'dateCreated:date',
+                 //'datePresented:date',
+                // 'committee1',
+                // 'committee2',
+                // 'committee3',
+                // 'RequestPDf',
 
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-    <?php if (Yii::$app->controller->id=="thesis"):?>
-    <h1><?= Html::encode($this->title) ?></h1><br>
-    <?php endif;?>
-    <p>Επιλέξτε το σύμβολο <span class="glyphicon glyphicon-eye-open" style="color:#0080ff"></span> στην δεξιά στήλη για να δείτε τα στοιχεία της διπλωματικής αναλυτικότερα.</p><br>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            //['class' => 'yii\grid\SerialColumn'],
-            'ID',
+                ['class' => 'yii\grid\ActionColumn',
+                    'template'=> '{view}',
+                    'controller'=>'thesis'
 
-            //'masterID',
-            [
-                'attribute' => 'masterID',
-                'value' => ('master.title')
+
+                ],
             ],
-
-            //'professorID',
-            [
-                'attribute' => 'professorID',
-                'value'=>function ($model) {
-                    return $model->professor->lastname.' '. $model->professor->firstname;
-                },
-            ],
-
-            'title',
-            //'description:ntext',
-            //'goal:ntext',
-            // 'prerequisite_knowledge:ntext',
-            // 'max_students',
-            // 'comments:ntext',
-             'status',
-             'dateCreated:date',
-             //'datePresented:date',
-            // 'committee1',
-            // 'committee2',
-            // 'committee3',
-            // 'RequestPDf',
-
-
-
-            ['class' => 'yii\grid\ActionColumn',
-                'template'=> '{view}',
-                'controller'=>'thesis'
-
-
-            ],
-        ],
-    ]); ?>
+        ]); ?>
+    </div>
 </div>
