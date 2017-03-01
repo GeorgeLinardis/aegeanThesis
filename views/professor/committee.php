fb5ffd4df9e6054d2a296aa44a3f44e0c0082898
GeorgeLinardis <glinardis@gmail.com>
2017-03-01 22:11:45 +0200

    added some explanation and changed title for clarification

--- views/professor/committee.php
+++ views/professor/committee.php
@@ -5,32 +5,34 @@ use yii\helpers\Html;
 /* @var $dataProvider \app\controllers\ThesisController*/
 ?>
 <?php
-$this->title = 'Επιτροπές Τρίτων';
+$this->title = 'Επιτροπές';
 $this->params['breadcrumbs'][] = ['label'=>'Καθηγητής','url'=>'main'];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="professor-committee">
-<h1> <?= Html::encode($this->title) ?></h1>
-Οι Επιτροπές στις οποίες λαμβάνετε μέρος την τρέχουσα περίοδο είναι οι εξής:<br><br>
+<div class="row">
+    <div class="professor-committee">
+    <h1> <?= Html::encode($this->title) ?></h1><br>
+    <p>Οι Επιτροπές στις οποίες λαμβάνετε μέρος ως επιβλέπων ή μέλος Επιτροπής την τρέχουσα περίοδο είναι οι εξής:</p>
+    <p>Επιλέξτε το σύμβολο <span class="glyphicon glyphicon-eye-open" style="color:#0080ff"></span> στην δεξιά στήλη για να δείτε τα στοιχεία της διπλωματικής αναλυτικότερα.</p><br><br>
 
 
+    <?=
 
-<?=
+    GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            'ID',
+            'masterID',
+            'title',
+            'status',
+            'datePresented',
+            ['class' => 'yii\grid\ActionColumn',
+                'template'=>'{view}',
+                'controller'=>'thesis'], //{delete} {update}
 
-GridView::widget([
-    'dataProvider' => $dataProvider,
-    'columns' => [
-        ['class' => 'yii\grid\SerialColumn'],
-        'ID',
-        'masterID',
-        'title',
-        'status',
-        'datePresented',
-        ['class' => 'yii\grid\ActionColumn',
-            'template'=>'{view}',
-            'controller'=>'thesis'], //{delete} {update}
+        ],
+    ]); ?>
 
-    ],
-]); ?>
-
+    </div>
 </div>
